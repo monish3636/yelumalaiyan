fc466f5da30a37d695bb1a8957127801a43ab52b
Mammattiyan <jibi343@gmail.com>
2014-06-07 12:19:46 +0530

    decomposition approve

--- application/modules/decomposition/controllers/decomposition.php
+++ application/modules/decomposition/controllers/decomposition.php
@@ -1,4 +1,4 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Decomposition extends MX_Controller{
    function __construct() {
                 parent::__construct();
