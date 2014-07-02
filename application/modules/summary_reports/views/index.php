28b6317a8790a08faec4caa142b9ea52e2dd5e46
Mammattiyan <jibi343@gmail.com>
2014-07-02 17:21:04 +0530

    Summary Report

---
+++ application/modules/summary_reports/views/index.php
@@ -0,0 +1,38 @@
+<style type="text/css">
+    .my_select{
+         -moz-border-bottom-colors: none;
+    -moz-border-left-colors: none;
+    -moz-border-right-colors: none;
+    -moz-border-top-colors: none;
+    background-color: #FFFFFF;
+    border-color: #C0C0C0 #D9D9D9 #D9D9D9;
+    border-image: none;
+    border-radius: 1px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    border-style: solid;
+    border-width: 1px;
+    box-shadow: none;
+    font-size: 13px;
+  
+    line-height: 1.4;
+    padding:1px 1px 1px 3px;
+    transition: none 0s ease 0s;
+    }
+ 
+</style>	
+
+<nav id="top_navigation">
+    <div class="container">
+            <div class="row">
+                <div class="col col-lg-4">
+<input id="select_branch" class="form-control" >
+                   
+               </div>
+            </div>
+    </div>
+</nav>
+<nav id="mobile_navigation"></nav>
+              
+</div>
+
+      
