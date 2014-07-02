d637360013e23d6d849f8da854649d3920cee061
Mammattiyan <jibi343@gmail.com>
2014-07-02 20:34:08 +0530

    Date Picker

--- application/modules/summary_reports/models/report.php
+++ application/modules/summary_reports/models/report.php
@@ -6,14 +6,14 @@ class Report extends CI_Model{
     }
     function search_branch($search){
         if($this->session->userdata('user_type')==2){
-            $this->db->select()->from('users_x_branches')->where('branches.active_status',1)->where('branches.delete_status',0);
+            $this->db->select('branches.store_name,branches.code,branches.guid')->from('users_x_branches')->where('branches.active_status',1)->where('branches.delete_status',0);
             $this->db->join('branches', 'branches.guid=users_x_branches.branch_id','left');
             $this->db->group_by('branches.guid');
             $sql=  $this->db->get();
             return $sql->result();
             
         }else{
-            $this->db->select()->from('users_x_branches')->where('users_x_branches.user_id',  $this->session->userdata('guid'))->where('branches.active_status',1)->where('branches.delete_status',0);
+            $this->db->select('branches.store_name,branches.code,branches.guid')->from('users_x_branches')->where('users_x_branches.user_id',  $this->session->userdata('guid'))->where('branches.active_status',1)->where('branches.delete_status',0);
             $this->db->join('branches', 'branches.guid=users_x_branches.branch_id','left');
             $sql=  $this->db->get();
             return $sql->result();
--- application/modules/summary_reports/views/index.php
+++ application/modules/summary_reports/views/index.php
@@ -18,21 +18,187 @@
     padding:1px 1px 1px 3px;
     transition: none 0s ease 0s;
     }
- 
+    section{
+        margin: 32px;
+    }
+    .table-condensed thead > tr > th, .table-condensed tbody > tr > th, .table-condensed tfoot > tr > th, .table-condensed thead > tr > td, .table-condensed tbody > tr > td, .table-condensed tfoot > tr > td {
+        padding: 3px 9px 2px;
+    }
 </style>	
+<script type="text/javascript">
+    function report(report){
+            var title=$('#'+report).text();
+            $('#title').text(title+' <?php echo $this->lang->line('report') ?>');
+            $('#'+report).offsetParent().addClass('active');
+    }
+</script>
 
 <nav id="top_navigation">
+
     <div class="container">
-            <div class="row">
-                <div class="col col-lg-4">
-<input id="select_branch" class="form-control" >
-                   
-               </div>
-            </div>
-    </div>
+					<ul id="text_nav_h" class="clearfix j_menu top_text_nav">
+					<li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('sales') ?></a>
+						<ul>
+                                                    <li><a href="javascript:report('sales_quotation')" id="sales_quotation"><?php echo $this->lang->line('sales_quotation') ?></a></li>
+							<li><a href="javascript:report('sales_order')" id='sales_order'><?php echo $this->lang->line('sales_order') ?></a></li>
+							<li><a href="javascript:report('sales_delivery_note')" id='sales_delivery_note'><?php echo $this->lang->line('sales_delivery_note') ?></a></li>
+							<li><a href="javascript:report('direct_sales_delivery_note')" id='direct_sales_delivery_note'><?php echo $this->lang->line('direct_sales_delivery_note') ?></a></li>
+							<li><a href="javascript:report('direct_sales')" id='direct_sales'><?php echo $this->lang->line('direct_sales') ?></a></li>
+							<li><a href="javascript:report('touch_sales')" id='touch_sales'><?php echo $this->lang->line('touch_sales') ?></a></li>
+							<li><a href="javascript:report('keyboard_sales')" id='keyboard_sales'><?php echo $this->lang->line('keyboard_sales') ?></a></li>
+							<li><a href="javascript:report('sales_bill')" id='sales_bill'><?php echo $this->lang->line('sales_bill') ?></a></li>
+							<li><a href="javascript:report('sales_return')" id='sales_return'><?php echo $this->lang->line('sales_return') ?></a></li>
+<!--							<li>
+								<a href="javascript:void(0)">Navigations</a>
+								<ul>
+									<li><a href="nav_side_accordion.html">Accordion Navigation</a></li>
+									<li class="link_active"><a href="nav_side_icons.html">Icon Navigation</a></li>
+								</ul>
+							</li>-->
+						</ul>
+					</li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('purchase') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('purchase_order') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('purchase_order_cancel') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('goods_receiving_note') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('direct_grn') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('direct_invoice') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('purchase_invoice') ?></a></li>
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('stock') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('opening_stock') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('stock_transfer') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('damage_stock') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('receiving_stock') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('stock_level') ?></a></li>
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('payment') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('supplier_payment') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('customer_payment') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('supplier_payable') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('customer_payable') ?></a></li>
+							
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('customers') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('customers') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('customer_category') ?></a></li>							
+							
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('suppliers') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('suppliers') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('suppliers_x_items') ?></a></li>							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('suppliers_category') ?></a></li>							
+							
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('decomposition') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('decomposition') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('decomposition_types') ?></a></li>							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('decomposition_items') ?></a></li>							
+							
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('item_kit') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('item_kit') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('kit_category') ?></a></li>							
+													
+							
+							
+                                                </ul>
+                                        </li>
+                                        <li>
+						<a href="javascript:void(0)"><?php echo $this->lang->line('users') ?></a>
+						<ul>
+							
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('users') ?></a></li>
+							<li><a href="javascript:report('')"><?php echo $this->lang->line('login_details') ?></a></li>							
+													
+							
+							
+                                                </ul>
+                                        </li>
+										
+                                        </ul>
+				</div>
 </nav>
 <nav id="mobile_navigation"></nav>
-              
-</div>
+<section>
+        <div class="row text-center">
+            <h4 id='title' ></h4>
+        </div>
+        <div class="row">
+            <div class="col col-lg-4">
+                <lable><?php  echo $this->lang->line('branches') ?></lable>
+                <input id="select_branch" class="form-control" >                   
+            </div>
+           <div class="col col-sm-4" >
+           <div class="form_sep">
+                    <label for="order_date" ><?php echo $this->lang->line('date_range') ?></label>													
+                             <div id="reportrange"  >
+                                <i class="icon icon-calendar "></i>
+                                <span><?php echo date("F j, Y", strtotime('-30 day')); ?> - <?php echo date("F j, Y"); ?></span> <b class="caret"></b>
+                            </div>
+                        </div>
+               </div>
+           </div>
+            
+        </div>
+    </section> 
+   
+         
 
+
+<script type="text/javascript">
+    $('#reportrange').daterangepicker(
+       {
+           ranges: {
+               'Today': [moment(), moment()],
+               'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
+               'Last 7 Days': [moment().subtract('days', 6), moment()],
+               'Last 30 Days': [moment().subtract('days', 29), moment()],
+               'This Month': [moment().startOf('month'), moment().endOf('month')],
+               'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
+           },
+           startDate: moment().subtract('days', 29),
+           endDate: moment()
+       },
+       function(start, end) {
+           $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+       }
+    );
+</script>
+      </div>
+</div>
+<script src="<?php echo base_url() ?>template/app/js/lib/jMenu/js/jMenu.jquery.js"></script>
       
