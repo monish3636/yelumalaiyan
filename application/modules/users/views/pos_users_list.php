af10ec00565fc8123fa552a4ef2a1e294c4b7e47
monish3636 <kmonish90@gmail.com>
2014-08-12 18:10:12 +0530

    fix 1

--- application/modules/users/views/pos_users_list.php
+++ application/modules/users/views/pos_users_list.php
@@ -1,22 +1,22 @@
 <style type="text/css">
     .my_select{
          -moz-border-bottom-colors: none;
-    -moz-border-left-colors: none;
-    -moz-border-right-colors: none;
-    -moz-border-top-colors: none;
-    background-color: #FFFFFF;
-    border-color: #C0C0C0 #D9D9D9 #D9D9D9;
-    border-image: none;
-    border-radius: 1px;
-    border: 1px solid rgba(0, 0, 0, 0.2);
-    border-style: solid;
-    border-width: 1px;
-    box-shadow: none;
-    font-size: 13px;
-  
-    line-height: 1.4;
-    padding:1px 1px 1px 3px;
-    transition: none 0s ease 0s;
+        -moz-border-left-colors: none;
+        -moz-border-right-colors: none;
+        -moz-border-top-colors: none;
+        background-color: #FFFFFF;
+        border-color: #C0C0C0 #D9D9D9 #D9D9D9;
+        border-image: none;
+        border-radius: 1px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        border-style: solid;
+        border-width: 1px;
+        box-shadow: none;
+        font-size: 13px;
+
+        line-height: 1.4;
+        padding:1px 1px 1px 3px;
+        transition: none 0s ease 0s;
     }
  
 </style>	
