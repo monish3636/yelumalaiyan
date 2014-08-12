b83b2a78f3b0a7b4b399a16acbfe5ead791862d0
monish3636 <kmonish90@gmail.com>
2014-08-12 17:29:37 +0530

    test

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://192.168.2.2/posnic/';
+$config['base_url']	= 'http://localhost/yelumalaiyan/';
 
 /*
 |--------------------------------------------------------------------------
